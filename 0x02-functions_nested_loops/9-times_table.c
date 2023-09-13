c2d60509679fc10409e1237ebe708748f53b1e98
XTAXME <thanbroad@gmail.com>
2023-09-12 17:58:48 -0700

    initial commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,41 +5,37 @@
  */
 void times_table(void)
 {
-	int row, col, product;
+    int row, col, product;
 
-	for (row = 0; row <= 9; row++)
-	{
-		for (col = 0; col <= 9; col++)
-		{
-			product = row * col;
+    for (row = 0; row <= 9; row++)
+    {
+        for (col = 0; col <= 9; col++)
+        {
+            product = row * col;
 
-			if (col == 0)
-			{
-				_putchar('0' + product);
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+            if (col == 0)
+            {
+                _putchar('0' + product);
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
 
-				if (product < 10)
-				{
-					_putchar(' '); /* Add an extra space for single-digit numbers */
-					_putchar('0' + product);
-				}
-				else
-				{
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-			}
+                if (product < 10)
+                {
+                    _putchar(' '); /* Add an extra space for single-digit numbers */
+                }
 
-			if (col < 9)
-			{
-				_putchar(' '); /* Add a space after each comma */
-			}
-		}
-		_putchar('\n');
-	}
+                _putchar('0' + product);
+            }
+
+            if (col < 9)
+            {
+                _putchar(' '); /* Add a space after each comma */
+            }
+        }
+        _putchar('\n');
+    }
 }
 
