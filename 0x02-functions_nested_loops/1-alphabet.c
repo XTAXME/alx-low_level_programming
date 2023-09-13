efd3f1cf9a91b3b8f7446aaa6548a9210ce7f5e0
XTAXME <thanbroad@gmail.com>
2023-09-12 17:04:31 -0700

    initial commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * main - function
- * Return: 0
+ * print_alphabet - prints the lowercase alphabet followed by a new line
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+    char letter = 'a';
 
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
 }
