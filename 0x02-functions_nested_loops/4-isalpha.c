e6f0e01ef6b49ca74bb3f64d049e3df2ab7799d5
XTAXME <thanbroad@gmail.com>
2023-09-12 17:23:57 -0700

    initial commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,18 @@
 #include "main.h"
-
-
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    int r;
-
-
-	    r = _isalpha('H');
-	    _putchar(r + '0');
-	    r = _isalpha('o');
-	    _putchar(r + '0');
-	    r = _isalpha(108);
-	    _putchar(r + '0');
-	    r = _isalpha(';');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    return (0);
-	}
+/**
+ * _isalpha - checks if a character is alphabetic
+ * @c: The character to be checked
+ *
+ * Return: 1 if the character is alphabetic, 0 otherwise
+ */
+int _isalpha(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
