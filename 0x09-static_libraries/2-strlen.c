2327f83111c2bb910e34acc3b510b55547f5865c
XTAXME <thanbroad@gmail.com>
2023-10-02 08:04:16 -0700

    initial commit

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * _strlen - returns the length of a string
  * @s: string
@@ -8,13 +8,11 @@ int _strlen(char *s)
 {
 	int longi = 0;
 
-
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
-
 
 	return (longi);
 }
